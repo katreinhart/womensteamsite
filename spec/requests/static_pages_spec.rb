de1a1301fab6492e5b0b3151603d1c0d502f924b
Kat Reinhart <kat.reinhart@gmail.com>
2013-10-30 12:39:44 -0700

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+	
+	describe "Home Page" do
+		
+		it "should have the content 'NW Womens Composite Team Info'" do
+			visit '/static_pages/home'
+			expect(page).to have_content('NW Womens Composite Team')
+		end
+		
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("NW Womens Composite Team | Home")
+		end
+	end
+	
+	describe "Help Page" do
+	
+		it "should have the content 'Help' " do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
+		end
+		
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title('NW Womens Composite Team | Help')
+		end
+	end
+	
+	describe "About Page" do
+		
+		it "should have the content 'About'" do
+			visit '/static_pages/about'
+			expect(page).to have_content('About')
+		end
+		
+		it "should have the title 'NW Womens Composite Team | About'" do
+			visit '/static_pages/about'
+			expect(page).to have_title('NW Womens Composite Team | About')
+		end
+	end
+end
